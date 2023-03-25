c502e8f282ef7094934102b92627d66ef4c9a083
Moon <moon.lee@mlytics.com>
2023-03-25 19:12:14 +0800

    [why] develop [what] apply player integration for dplayer

--- DPlayer/react-sample/src/components/Player.js
+++ DPlayer/react-sample/src/components/Player.js
@@ -1,56 +1,42 @@
-import { useEffect, useRef } from 'react';
+import React, {
+    useEffect,
+    useRef
+} from 'react';
 
-import DPlayer from 'dplayer';
-import Hls from 'hls.js';
-import mux from 'mux-embed';
-
-import { HLSLoader } from '@mlytics/p2sp-sdk/driver/integration/streaming/hls';
+import {
+    driver
+} from '@mlytics/p2sp-sdk/driver/peripheral/player/dplayer/streaming/hls/bundle';
 
 const Player = (props) => {
-  const videoRef = useRef(null);
-  const dpRef = useRef(null);
+    const videoRef = useRef(null);
+    const dpRef = useRef(null);
+    const {
+      options
+    } = props
 
-  useEffect(() => {
-    const { source, autoplay, controls } = props.options;
-    const { src } = source;
-
-    const video = videoRef.current;
-    dpRef.current = new DPlayer({
-      container: video,
-      autoplay,
-      controls,
-      video: {
-        url: src,
-        type: 'customHls',
-        customType: {
-          customHls: (video) => {
-            const hls = new Hls({
-              loader: HLSLoader
-            });
-            hls.loadSource(video.src);
-            hls.attachMedia(video);
-
-            mux.monitor(video, {
-              Hls: Hls,
-              hlsjs: hls,
-              ...props.options.mux
-            });
-          }
+    useEffect(() => {
+        const video = videoRef.current;
+        if (!dpRef.current) {
+          const adapter = driver.extensions.DPlayerHlsPlayerPlugin.create({
+            container: video,
+            ...options
+          });
+          dpRef.current = adapter.player;
         }
-      }
-    });
+    }, [videoRef]);
 
-  }, [videoRef]);
-
-  useEffect(() => {
-    const dp = dpRef.current;
-    return () => {
-      if (dp) dp.destroy();
-    }
-  }, [dpRef]);
+    useEffect(() => {
+        const dp = dpRef.current;
+        return () => {
+          if (dp) {
+            dp.destroy();
+            dpRef.current = null;
+          }
+        };
+    }, [dpRef]);
 
   return (
-    <div id="video" ref={videoRef} style={{width: "100%", maxWidth: "800px"}}/>
+    <div id="video" ref={videoRef} style={{width: "100%", maxWidth: "800px"}} />
   );
-}
+};
 export default Player;
