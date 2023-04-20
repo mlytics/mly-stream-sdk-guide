d6bff9672406e8df5ddf63b63eb153dd68ccd66c
patricelee <patrice.lee@mlytics.com>
2023-04-20 08:21:13 +0800

    [why]update[what]refactor build script

--- .rdme-deploy/build.sh
+++ .rdme-deploy/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-json_path=./.rdme-deploy/meta.json
-mkdir -p dist/docs
+json_path=.rdme-deploy/meta.json
+output=dist/docs
+rm -rf $output
+mkdir -p $output
 
 jq -c '.[]' $json_path | while read item;
 do
@@ -14,19 +16,5 @@ title: '${title}'
 category: ${category}
 slug: ${slug}
 ---"
-    echo "$content" | cat - "$file_path" > temp && mv temp dist/docs/$category-$slug.md
+    echo "$content" | cat - "$file_path" > temp && mv temp $output/$category-$slug.md
 done
-
-# for item in $(echo "$json_data" | jq -c '.[]'); do
-#     file_path=$(echo "$item" | jq -r '.file_path')
-#     title=$(echo "$item" | jq -r '.title')
-#     category=$(echo "$item" | jq -r '.category')
-#     slug=$(echo "$item" | jq -r '.slug')
-
-#     content="---
-# title: '${title}'
-# category: ${category}
-# slug: ${slug}
-# ---"
-#     echo "$content" | cat - $file_path > temp && mv temp dist/docs/$category-$slug.md
-# done
